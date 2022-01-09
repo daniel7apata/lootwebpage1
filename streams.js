24da610c195953f701d0d5f71f39716cbea8e299
daza9604 <90068737+daza9604@users.noreply.github.com>
2022-01-09 03:58:24 -0500

    Create streams.js

---
+++ streams.js
@@ -0,0 +1,71 @@
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "twzerman",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+           
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "neol3gend",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });  
+
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "michantoo",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });          
+        
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "stuuckyt_",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+     
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "leu_w",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+ 
+        
+
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "darehumanz",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });      
+ 
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "marc_pleasant",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+
+      new Twitch.Embed("twitch-embed", {
+        width: 400,
+        height: 550,
+        channel: "dazastr",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
